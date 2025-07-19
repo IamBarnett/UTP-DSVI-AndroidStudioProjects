3a208ba904f4ba45961c44d4851375eb2f0852b3
IamBarnett <luis.barnett1@utp.ac.pa>
2025-07-19 13:48:54 -0500

    delete to renew

--- MindSparkAI/app/src/main/java/com/universidad/mindsparkai/data/models/ChatMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.universidad.mindsparkai.data.models
-
-data class ChatMessage(
-    val id: String = "",
-    val content: String = "",
-    val isFromUser: Boolean = true,
-    val timestamp: Long = System.currentTimeMillis(),
-    val aiModel: String = "GPT-4"
-)
